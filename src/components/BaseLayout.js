7804e9165c544879c9c57d666cf1f7479927c17e
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-31 10:37:11 -0400

    setup baselayout inline styles

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+export default class BaseLayout extends Component {
+  render(){
+
+    let headerStyle = {
+      backgroundColor: "gray",
+      height: 70
+    }
+
+    let ulStyle = {
+      listStyle: "none",
+      margin: 0,
+      padding: 26,
+    }
+
+    let liStyle = {
+      display: "inline",
+      marginTop: "15px"
+    }
+
+    let linkStyle = {
+      border: "1px solid #E5B13A",
+      borderRadius: 90,
+      padding: "15px",
+      marginRight: "10px",
+      textDecoration: "none",
+      color: "#E5B13A",
+      backgroundColor: "black",
+
+    }
+
+    let footerStyle = {
+      textAlign: "center",
+      backgroundColor: "lightgray",
+      height: 40,
+      margin: "0 auto"
+    }
+
+    return(
+      <div>
+        <div style={headerStyle}>
+          <ul style={ulStyle}>
+            <li style={liStyle}>
+              <Link to="/" style={linkStyle}>Home</Link>
+            </li>
+            <li style={liStyle}>
+              <Link to="/about" style={linkStyle}>About</Link>
+            </li>
+            <li style={liStyle}>
+              <Link to="/portfolio" style={linkStyle}>Portfolio</Link>
+            </li>
+          </ul>
+        </div>
+        <div style={{backgroundColor: "black", height: "80vh"}}>
+          {this.props.children}
+        </div>
+        <footer style={footerStyle}>
+        </footer>
+      </div>
+    );
+  }
+}
